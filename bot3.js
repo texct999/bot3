3ba4fa23b4442d5b1149b0a9635dae5efdc7dc4e
texct999 <45326466+texct999@users.noreply.github.com>
2018-11-25 08:44:54 +0200

    Add files via upload

---
+++ bot3.js
@@ -0,0 +1,167 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'هلا') {
+    msg.reply('اهلين');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'السلام عليكم') {
+    msg.reply('وعليكم السلام ورحمة الله وبركاتة');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '.') {
+    msg.reply('فديت الى ينقط');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'باك') {
+    msg.reply('ولكم');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'ميسي') {
+    msg.reply('ميسى مش فاضى لك بيلعب ماتش');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'برب') {
+    msg.reply('روح حب ولا تطول');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'سلام') {
+    msg.reply('حب روح ولا تطول ');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'help') {
+    msg.reply('هل يمكننى مساعدتك');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'هاى') {
+    msg.reply('هايات منور');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+
+client.on('message', msg => {
+  if (msg.content === 'ههه') {
+    msg.reply(':relaxed: :relaxed: :relaxed: مافى اشى يضحك');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'الشعار') {
+    msg.reply('هنا اسمك( Ɲιя )');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'ماما') {
+    msg.reply('عطنى بوسة يا ولدى امووووةةةة');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'احب برو بوت') {
+    msg.reply('حرام وناا ما تحبنى ليش ليش وش سويتلك منشان تخونى واء واء');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'تيت.') {
+    msg.reply('تيت');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'الله') {
+    msg.reply('سبحان الله بحمدة وسبحان الله العظيم');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'لا اله الا الله') {
+    msg.reply('محمد رسول الله');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'اللهم صلى على محمد') {
+    msg.reply('عليه افضل الصلات والسلام');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === 'احبك بوت') {
+    msg.reply('عيب خجلتنى انا بنت  اوووم ماى جات قولها ثانى بليز');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
+
+client.on('message', msg => {
+  if (msg.content === ' شعر حب') {
+    msg.reply('احبك لو قلبى مات احبك لوروحى فداكى احبك من قلبى الغالى احبك ونتى ع حضنى الغالى');
+  }
+});
+
+client.login('NTE1OTM1NTk4NjkyMDA3OTQw.Dtsi1A.VNV7RfPItsIwh6HO8MQFbAd_r4o');
